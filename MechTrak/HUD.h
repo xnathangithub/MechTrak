7bf69d3ee0e0bc954228b96a658dac9e7fb7c7ec
Nathan <90913078+xnathangithub@users.noreply.github.com>
2026-02-21 01:40:48 -0500

    Update HUD.h

--- MechTrak/HUD.h
+++ MechTrak/HUD.h
@@ -1,41 +1,42 @@
 #pragma once
-#include "bakkesmod/plugin/bakkesmodplugin.h"
-#include "bakkesmod/wrappers/canvaswrapper.h"
-#include <map>
-#include <string>
+#include "../plugin/bakkesmodsdk.h"
 #include <vector>
+#include <typeindex>
+#include <string>
+#include "cvarwrapper.h"
 
-struct ShotStats {
-    int attempts = 0;
-    int goals = 0;
-    std::vector<bool> attemptHistory;
-};
+typedef void (*commandNotifier)(std::vector<std::string>);
 
-class HUD {
+class BAKKESMOD_PLUGIN_IMPORT CVarManagerWrapper
+{
 public:
-    static void Render(
-        CanvasWrapper& canvas,
-        std::shared_ptr<CVarManagerWrapper> cvarManager,
-        std::shared_ptr<GameWrapper> gameWrapper,
-        std::map<int, ShotStats>& shotStats,
-        std::map<int, std::string>& shotTypes,
-        int currentShotNumber,
-        bool sessionActive
-    );
+	CVarManagerWrapper(std::uintptr_t mem, std::type_index pluginIdx);
+	CVarManagerWrapper(const CVarManagerWrapper& other);
+	CVarManagerWrapper& operator=(CVarManagerWrapper rhs);
+	~CVarManagerWrapper();
 
-    static void RenderCompact(
-        CanvasWrapper& canvas,
-        std::map<int, ShotStats>& shotStats,
-        std::map<int, std::string>& shotTypes,
-        int currentShotNumber,
-        bool sessionActive
-    );
+	void executeCommand(std::string command, bool log = true);
+	void registerNotifier(std::string cvar, commandNotifier notifier, std::string description, unsigned char permissions);
+	void registerNotifier(std::string cvar, std::function<void(std::vector<std::string>)> notifier, std::string description, unsigned char permissions);
+	bool removeNotifier(std::string cvar);
 
-    static void DrawMiniGraph(
-        CanvasWrapper& canvas,
-        std::map<int, ShotStats>& shotStats,
-        int currentShotNumber,
-        int startX,
-        int startY
-    );
+	CVarWrapper registerCvar(std::string cvar, std::string defaultValue, std::string desc = "", bool searchAble = true, bool hasMin = false, float min = 0, bool hasMax = false, float max = 0, bool saveToCfg = true);
+	bool removeCvar(std::string cvar);
+
+	void log(std::string text);
+    void log(std::wstring text);
+	CVarWrapper getCvar(std::string cvar);
+
+	std::string getBindStringForKey(std::string key);
+	void setBind(std::string key, std::string command);
+	void removeBind(std::string key);
+	std::string getAlias(std::string alias);
+	void setAlias(std::string key, std::string script);
+
+	void backupCfg(std::string path);
+	void backupBinds(std::string path);
+	void loadCfg(std::string path);
+private:
+    PIMPL
 };
+
